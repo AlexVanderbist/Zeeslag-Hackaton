9cb1740895ad1c011423c776775be474beb1ffc1
rowanvanekeren <rowanvanekeren@hotmail.com>
2016-10-21 18:48:18 +0200

    js file bijgewerkt

--- front-end_gsm/js/main.js
+++ front-end_gsm/js/main.js
@@ -1,3 +1,32 @@
 /**
  * Created by Rowan on 21-10-2016.
  */
+(function(){
+    var app = angular.module("mainApp",[]);
+
+    app.controller('MainController',['$rootScope','$scope','$http',function($rootScope,$scope,$http){
+
+        $scope.gameStatus = 0;
+
+        $scope.initWebsite = function(){
+            $http({
+                method: 'GET',
+                url: '/game'
+            }).then(function successCallback(response) {
+                $scope.gameStatus = response['status'];
+                if(response['status'] == 2){
+                    window.location = "./gridGame.html"
+                }
+
+            }, function errorCallback(response) {
+
+            });
+        }
+
+
+        $scope.get
+
+
+    }]);
+
+})();
